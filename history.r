753ad6a03a0682e39da558f96fb1570025726ed7
julien colomb <julien.colomb@hu-berlin.de>
2019-08-12 14:52:19 +0200

    add history info, and toml file

---
+++ history.r
@@ -0,0 +1,16 @@
+blogdown::new_site(theme = "devcows/hugo-universal-theme")
+
+download.file(
+
+"https://github.com/jcolomb/hugrid/archive/master.zip",
+destfile ="themes/hugrid.zip")
+
+unzip("themes/hugrid.zip", exdir= "themes")
+
+#delete zip file manually
+#copy and rename confighug.toml
+# copy item.toml and images
+# modify confighug.toml, see commit history
+
+blogdown::hugo_cmd("--config confighug.toml") #bulid dat version
+blogdown::serve_site()
